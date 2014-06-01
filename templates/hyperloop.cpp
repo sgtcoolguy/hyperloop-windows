b79cd3295ca91f6de6896c3ae81ee7e32b7f6713
Kota Iguchi <developer@infosia.co.jp>
2014-06-01 22:46:29 +0900

    Fix: compile error at generateApp

--- templates/hyperloop.cpp
+++ templates/hyperloop.cpp
@@ -4,7 +4,51 @@
 
 #include <iostream>
 #include <string>
+#include <inspectable.h>
 
+/*
+ * Windows object specialization
+ */
+
+template <>
+void Hyperloop::NativeObject<IInspectable*>::release()
+{
+
+}
+
+template <>
+void Hyperloop::NativeObject<IInspectable*>::retain()
+{
+
+}
+
+template<>
+bool Hyperloop::NativeObject<IInspectable*>::hasInstance(JSContextRef ctx, JSValueRef other, JSValueRef* exception)
+{
+	return false;
+}
+
+template<>
+std::string Hyperloop::NativeObject<IInspectable*>::toString(JSContextRef ctx, JSValueRef* exception)
+{
+	return nullptr;
+}
+
+template<>
+double Hyperloop::NativeObject<IInspectable*>::toNumber(JSContextRef ctx, JSValueRef* exception)
+{
+	return NAN;
+}
+
+template<>
+bool Hyperloop::NativeObject<IInspectable*>::toBoolean(JSContextRef ctx, JSValueRef* exception)
+{
+	return false;
+}
+
+IInspectable* HyperloopWindowsObjectToPointer(Platform::Object^ o) {
+	return reinterpret_cast<IInspectable*>(o);
+}
 
 /**
  * native implementation of the logger
--- templates/hyperloop.h
+++ templates/hyperloop.h
@@ -5,7 +5,9 @@
 #ifndef __HYPERLOOPWIN__HEADER__
 #define __HYPERLOOPWIN__HEADER__
 
+#include <inspectable.h>
 
+IInspectable* HyperloopWindowsObjectToPointer(Platform::Object^ o);
 
 #endif
  
