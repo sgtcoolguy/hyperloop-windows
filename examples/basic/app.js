1288d865f72051e20bf7e553be81e54cb98f76ab
Kota Iguchi <developer@infosia.co.jp>
2014-06-03 07:47:51 +0900

    Implement method, getter and setter

--- examples/basic/app.js
+++ examples/basic/app.js
@@ -1,15 +1,15 @@
 "use hyperloop"
 
 var window = Windows.UI.Xaml.Window.Current;
-/*
-var text = new TextBlock();
+var text = new Windows.UI.Xaml.Controls.TextBlock();
 
 text.Text = "Hello, world!";
+/*
 text.TextAlignment = TextAlignment.Center;
 text.VerticalAlignment = VerticalAlignment.Center;
 text.HorizontalAlignment = HorizontalAlignment.Center;
+*/
 text.FontSize = 60;
 
 window.Content = text;
-*/
 window.Activate();
