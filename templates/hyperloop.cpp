b606967c849bdfa311a9b4db6ebc98acfecb7385
Kota Iguchi <developer@infosia.co.jp>
2014-06-04 08:06:12 +0900

    Add windows object specialization template Add preprocessor macros for JavaScriptCore

--- templates/hyperloop.cpp
+++ templates/hyperloop.cpp
@@ -6,46 +6,6 @@
 #include <string>
 #include <inspectable.h>
 
-/*
- * Windows object specialization
- */
-
-template <>
-void Hyperloop::NativeObject<Platform::Object^>::release()
-{
-
-}
-
-template <>
-void Hyperloop::NativeObject<Platform::Object^>::retain()
-{
-
-}
-
-template<>
-bool Hyperloop::NativeObject<Platform::Object^>::hasInstance(JSContextRef ctx, JSValueRef other, JSValueRef* exception)
-{
-	return false;
-}
-
-template<>
-std::string Hyperloop::NativeObject<Platform::Object^>::toString(JSContextRef ctx, JSValueRef* exception)
-{
-	return nullptr;
-}
-
-template<>
-double Hyperloop::NativeObject<Platform::Object^>::toNumber(JSContextRef ctx, JSValueRef* exception)
-{
-	return NAN;
-}
-
-template<>
-bool Hyperloop::NativeObject<Platform::Object^>::toBoolean(JSContextRef ctx, JSValueRef* exception)
-{
-	return false;
-}
-
 IInspectable* HyperloopWindowsObjectToPointer(Platform::Object^ o) {
 	return reinterpret_cast<IInspectable*>(o);
 }
--- templates/hyperloop.h
+++ templates/hyperloop.h
@@ -7,6 +7,44 @@
 
 #include <inspectable.h>
 
+typedef Hyperloop::NativeObject<Platform::Object^> * NativeWindowsObject;
+
+template<class T>
+void Hyperloop::NativeObject<T>::release()
+{
+
+}
+
+template<class T>
+void Hyperloop::NativeObject<T>::retain()
+{
+
+}
+
+template<class T>
+bool Hyperloop::NativeObject<T>::hasInstance(JSContextRef ctx, JSValueRef other, JSValueRef* exception)
+{
+	return false;
+}
+
+template<class T>
+std::string Hyperloop::NativeObject<T>::toString(JSContextRef ctx, JSValueRef* exception)
+{
+	return nullptr;
+}
+
+template<class T>
+double Hyperloop::NativeObject<T>::toNumber(JSContextRef ctx, JSValueRef* exception)
+{
+	return 0;
+}
+
+template<class T>
+bool Hyperloop::NativeObject<T>::toBoolean(JSContextRef ctx, JSValueRef* exception)
+{
+	return false;
+}
+
 IInspectable* HyperloopWindowsObjectToPointer(Platform::Object^ o);
 
 #endif
