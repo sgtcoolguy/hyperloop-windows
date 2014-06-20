9610b9ab0dbe2a1e287037ee5f11ddd0bfca7bdb
Kota Iguchi <developer@infosia.co.jp>
2014-06-20 18:40:24 +0900

    Add support for Platform::ValueType (as well as struct)

---
+++ examples/buttonAlert/app.js
@@ -0,0 +1,34 @@
+"use hyperloop"
+
+var window = Windows.UI.Xaml.Window.Current,
+	grid = new Windows.UI.Xaml.Controls.Grid(),
+	button = new Windows.UI.Xaml.Controls.Button(),
+	white = new Windows.UI.Xaml.Media.SolidColorBrush(Windows.UI.Colors.White),
+	text = new Windows.UI.Xaml.Controls.TextBlock();
+
+text.Text = "Click me, please!";
+text.TextAlignment = Windows.UI.Xaml.TextAlignment.Center;
+text.VerticalAlignment = Windows.UI.Xaml.VerticalAlignment.Center;
+text.HorizontalAlignment = Windows.UI.Xaml.HorizontalAlignment.Center;
+text.FontSize = 60;
+
+grid.Width = window.Bounds.Width;
+grid.Height = window.Bounds.Height;
+
+button.Content = text;
+button.Width = 500;
+button.Height = 100;
+button.Foreground = white;
+button.VerticalAlignment = Windows.UI.Xaml.VerticalAlignment.Center;
+button.HorizontalAlignment = Windows.UI.Xaml.HorizontalAlignment.Center;
+grid.Children.Append(button);
+
+/* function callback is not working yet
+grid.add_Tapped(function() {
+	var dialog = new Windows.UI.Popups.MessageDialog("The Message", "The Title");
+		dialog.ShowAsync();
+});
+*/
+
+window.Content = grid;
+window.Activate();
---
+++ examples/valuetype/app.js
@@ -0,0 +1,3 @@
+"use hyperloop"
+
+var white = new Windows.UI.Xaml.Media.SolidColorBrush(Windows.UI.Colors.White);
