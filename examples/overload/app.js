cd8efd0cbaba4dfa60c277af8b3c9a63912f7607
Kota Iguchi <developer@infosia.co.jp>
2014-07-23 15:58:10 +0900

    Update app.js

--- examples/overload/app.js
+++ examples/overload/app.js
@@ -1,5 +1,9 @@
 "use hyperloop"
 
+/*
+ * This examples does nothing useful but just for testing transformation.
+ */
+
 var window = Windows.UI.Xaml.Window.Current;
 var text = new Windows.UI.Xaml.Controls.TextBlock();
 
@@ -13,7 +17,6 @@ text.FontSize = 60;
 
 Hyperloop.method(window, 'Equals(object)').call(text);
 Hyperloop.method('Platform.Object', 'ReferenceEquals(object,object)').call(window, text);
-var a = new Platform.Metadata.DefaultMemberAttribute('test');
 
 window.Content = text;
 window.Activate();
